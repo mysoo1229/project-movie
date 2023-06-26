f974d517472d88dff9bd1477a8dd593f37972969
Crystal <crystalyoo29@interpark.com>
2023-06-26 16:56:49 +0900

    [install] styled-components 설치 후 global style과 theme 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import GlobalStyle from './GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { myTheme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <>
+  <ThemeProvider theme={myTheme}>
+    <GlobalStyle />
     <App />
-  </>
+  </ThemeProvider>
 );
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+export const myTheme:DefaultTheme = {
+  black: {
+    darker: "#111",
+    lighter: "#333",
+  },
+  white: {
+    lighter: "#fff",
+    darker: "#efefef",
+  },
+};
